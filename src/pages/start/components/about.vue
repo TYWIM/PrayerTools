f68a5159116e0ce22c267b9443dbd06da4d31016
承心 <117726202+TYWIM@users.noreply.github.com>
2025-02-18 21:11:16 +0800

    Update about.vue

--- src/pages/start/components/about.vue
+++ src/pages/start/components/about.vue
@@ -1,13 +1,13 @@
 <template>
   <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-    <img src="https://socialify.git.ci/lctoolsweb/LunarCoreTools/image?description=1&forks=1&issues=1&language=1&logo=https%3A%2F%2Fimg.morax.top%2Ffile%2F2578855f05450d5878252.png&name=1&pulls=1&stargazers=1&theme=Light" alt="LunarCoreTools" style="width: 100%; height: auto;" />
+    <img src="https://socialify.git.ci/TYWIM/PrayerTools/image?description=1&forks=1&issues=1&language=1&logo=https%3A%2F%2Fimg.morax.top%2Ffile%2F2578855f05450d5878252.png&name=1&pulls=1&stargazers=1&theme=Light" alt="LunarCoreTools" style="width: 100%; height: auto;" />
     <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; font-size: 20px; margin-top: 50px;">
       <span style="font-size: 30px;">免责声明</span>
       <span style="margin-top: 10px; font-size: 20px;">本工具与任何公开的服务器没有从属关系，请您注意甄别</span>
-      <span style="margin: 10px; font-size: 20px;">本项目只是用于学习TypeScript与vue相关的知识，切勿用于非法用途。请不要在无关的地方宣传LunarCoreTools。</span>
+      <span style="margin: 10px; font-size: 20px;">本项目只是用于学习TypeScript与vue相关的知识，切勿用于非法用途。请不要在无关的地方宣传PrayerTools。</span>
       <span style="margin: 10px; font-size: 20px;">Q群仅用作闲聊，如果你有任何使用问题，请前往GitHub提交issue，而不是在Q群询问</span>
     </div>
-    <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=J54m8XB3Ig8VJ41ICO8KW029aSz4fFB-&authKey=tq9%2BSsg2M30Jy1v9OwJEa%2B%2FmarnH2AslQYQsv24BkkeqY39%2FtlpZsrIkqO01SScg&noverify=0&group_code=124750952" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
+    <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=J54m8XB3Ig8VJ41ICO8KW029aSz4fFB-&authKey=tq9%2BSsg2M30Jy1v9OwJEa%2B%2FmarnH2AslQYQsv24BkkeqY39%2FtlpZsrIkqO01SScg&noverify=0&group_code=682921184" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
       <button style="background-color: white; border: 2px solid blue; color: blue; padding: 15px 32px; text-align: center; text-decoration: none; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 20px; display: flex; justify-content: center; align-items: center;">加入Q群</button>
     </a>
   </div>
